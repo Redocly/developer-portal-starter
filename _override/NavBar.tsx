07829d45128c33e006fffb30a4ce6c5cac71e043
Roman Hotsiy <gotsijroman@gmail.com>
2020-07-20 19:28:12 +0300

    chore: prettier

--- _override/NavBar.tsx
+++ _override/NavBar.tsx
@@ -4,43 +4,45 @@ import styled from 'styled-components';
 import { Flex, Link, SearchBox } from '@redocly/ui';
 
 export default function NavBar(props) {
-    const {items, logo, location} = props;
-    const isMain = location.pathname !== '/'; // Change the color of the NavBar based on location
+  const { items, logo, location } = props;
+  const isMain = location.pathname !== '/'; // Change the color of the NavBar based on location
 
-    return (
-        <NavWrapper hasBackground={isMain}>
-            <Flex p="20px">
-                <img src={logo} alt="" height="50"/>
-                <NavItems>
-                    {
-                        items.filter(item => item.type !== 'search').map((item, index) => {
-                            return <li key={index}>
-                                <Link to={item.link}>{item.label}</Link>    
-                            </li>
-                        })
-                    }
-                </NavItems>
-                <SearchBox />
-            </Flex>
-        </NavWrapper>
-    )
+  return (
+    <NavWrapper hasBackground={isMain}>
+      <Flex p="20px">
+        <img src={logo} alt="" height="50" />
+        <NavItems>
+          {items
+            .filter(item => item.type !== 'search')
+            .map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link to={item.link}>{item.label}</Link>
+                </li>
+              );
+            })}
+        </NavItems>
+        <SearchBox />
+      </Flex>
+    </NavWrapper>
+  );
 }
 
 const NavWrapper = styled.div<{ hasBackground: boolean }>`
-    background: ${({hasBackground}) => hasBackground ? '#227a88' : 'transparent'};
+  background: ${({ hasBackground }) => (hasBackground ? '#227a88' : 'transparent')};
 `;
 
 const NavItems = styled.ul`
-    margin: 0 0 0 40px;
-    padding: 0;
-    display: flex;
-    align-items: center;
-    justify-content: start;
-    & li {
-        list-style: none;
-        margin-right: 20px;
-        & a {
-            color: #FFFFFF;
-        }
+  margin: 0 0 0 40px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  & li {
+    list-style: none;
+    margin-right: 20px;
+    & a {
+      color: #ffffff;
     }
+  }
 `;
