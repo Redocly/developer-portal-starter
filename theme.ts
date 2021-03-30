41e5a399be3852301835d2a85716c98baafe418d
Tessa Kriesel <me@tessak22.com>
2021-03-30 08:22:59 -0500

    try new theme.ts again

--- theme.ts
+++ theme.ts
@@ -1,53 +1,75 @@
 import { lighten, darken, readableColor } from 'polished';
 
 export const theme = {
-
+  // spacing: {
+  //   unit: 5,
+  //   sectionHorizontal: ({ spacing }) => spacing.unit * 8,
+  //   sectionVertical: ({ spacing }) => spacing.unit * 8,
+  // },
+  // breakpoints: {
+  //   xs: 0,
+  //   small: '550px',
+  //   medium: '900px',
+  //   large: '1200px',
+  // },
   colors: {
-    palette: {
-      black: '#000000',
-      grey1: '#474747',
-      grey2: '#828282',
-      grey3: '#BBBBBB',
-      grey4: '#E0E4EA',
-      grey5: '#F0F2F5',
-      white: '#FFFFFF',
-      blue: '#0286FF',
-      red: '#EB5757',
-      green: '#24B560',
-      pink: '#F6C5CF'
+    // tonalOffset: 0.2,
+    primary: {
+      main: '#000000',
+      // light: ({ colors }) => lighten(colors.tonalOffset, colors.primary.main),
+      // dark: ({ colors }) => darken(colors.tonalOffset, colors.primary.main),
+      // contrastText: ({ colors }) => readableColor(colors.primary.main),
     },
-    action: {
-      success: ({ colors }) => colors.palette.green,
-      error: ({ colors }) => colors.palette.red,
-      link: ({ colors }) => colors.palette.blue,
-      inactive: ({ colors }) => colors.palette.grey3,
-      hover: ({ colors }) => colors.palette.grey4,
-      // warning: X
-    },
-    brand: {
-      black: ({ colors }) => colors.palette.black,
-      white: ({ colors }) => colors.palette.white,
-      blue: ({ colors }) => colors.palette.blue,
-      red: ({ colors }) => colors.palette.red,
-      green: ({ colors }) => colors.palette.green,
-      pink: ({ colors }) => colors.palette.pink,
-    },
+    // success: {
+    //   main: '#00aa13',
+    //   light: ({ colors }) => lighten(colors.tonalOffset * 3, colors.success.main),
+    //   dark: ({ colors }) => darken(colors.tonalOffset, colors.success.main),
+    //   contrastText: ({ colors }) => readableColor(colors.success.main),
+    // },
+    // error: {
+    //   main: '#e53935',
+    //   light: ({ colors }) => lighten(colors.tonalOffset * 2, colors.error.main),
+    //   dark: ({ colors }) => darken(colors.tonalOffset, colors.error.main),
+    //   contrastText: ({ colors }) => readableColor(colors.error.main),
+    // },
+    // warning: {
+    //   main: '#d4ad03',
+    //   light: ({ colors }) => lighten(colors.tonalOffset * 2, colors.warning.main),
+    //   dark: ({ colors }) => darken(colors.tonalOffset, colors.warning.main),
+    //   contrastText: ({ colors }) => readableColor(colors.warning.main),
+    // },
+    // info: {
+    //   main: '#4782cb',
+    //   light: ({ colors }) => lighten(colors.tonalOffset * 2, colors.info.main),
+    //   dark: ({ colors }) => darken(colors.tonalOffset, colors.info.main),
+    //   contrastText: ({ colors }) => readableColor(colors.info.main),
+    // },
     text: {
-      primary: ({ colors }) => colors.palette.black,
-      base: ({ colors }) => colors.palette.grey1,
-      secondary: ({ colors }) => colors.palette.grey2,
-      inactive: ({ colors }) => colors.action.inactive,
+      primary: '#424242',
+      // secondary: '#4e566d',
     },
-    icon: {
-      background: ({ colors }) => colors.palette.grey5,
-      inactive: ({ colors }) => colors.action.inactive,
-      actionable: ({ colors }) => colors.action.link,
-      base: ({ colors }) => colors.text.base,
-      hover: ({ colors }) => colors.action.hover,
-    },
-    background: {
-      flush: ({ colors }) => colors.palette.grey5,
-    },
+    // border: {
+    //   dark: 'rgba(0,0,0, 0.15)',
+    //   light: '#ffffff',
+    // },
+    // responses: {
+    //   success: {
+    //     color: ({ colors }) => colors.success.main,
+    //     backgroundColor: ({ colors }) => transparentize(0.9, colors.success.main),
+    //   },
+    //   error: {
+    //     color: ({ colors }) => colors.error.main,
+    //     backgroundColor: ({ colors }) => transparentize(0.9, colors.error.main),
+    //   },
+    //   redirect: {
+    //     color: ({ colors }) => colors.warning.main,
+    //     backgroundColor: ({ colors }) => transparentize(0.9, colors.responses.redirect.color),
+    //   },
+    //   info: {
+    //     color: ({ colors }) => colors.info.main,
+    //     backgroundColor: ({ colors }) => transparentize(0.9, colors.responses.info.color),
+    //   },
+    // },
     http: {
       get: '#6bbd5b',
       post: '#248fb2',
@@ -59,118 +81,104 @@ export const theme = {
       link: '#31bbb6',
       head: '#c167e4',
     },
+    // navbar: {
+    //   main: ({ colors }) => colors.primary.main,
+    //   gradient: ({ colors }) => darken(colors.tonalOffset / 2, colors.navbar.main),
+    //   contrastText: 'white'
+    // },
+    // footer: {
+      // main: ({ colors }) => colors.primary.main,
+      // contrastText: 'white'
+    // },
   },
+
   sidebar: {
     backgroundColor: '#fafafa',
     width: '260px',
   },
+  // tocPanel: {
+  //   width: '240px',
+  // },
+
   typography: {
-    fontSize: '1rem',
-    lineHeight: '1.5rem',
-    fontFamily: 'Inter',
-    fontWeight: {
-      bold: '600',
-      semibold: '500',
-      regular: '400',
-      light: '300',
+    fontSize: '16px',
+    lineHeight: '1.5em',
+    fontWeightRegular: '400',
+    fontWeightBold: '600',
+    fontWeightLight: '300',
+    fontFamily: '"Source Sans Pro", sans-serif',
+    headings: {
+      fontFamily: '"Source Sans Pro", sans-serif',
+      fontWeight: '600',
     },
-    heading: {
-      fontFamily: ({ typography }) => typography.fontFamily,
-      lineHeight: '1.5rem',
-      large: {
-        fontSize: '1.75rem',
-        fontWeight: ({ typography }) => typography.fontWeight.bold,
-        fontFamily: ({ typography }) => typography.heading.fontFamily,
-        lineHeight: ({ typography }) => typography.heading.lineHeight,
-      },
-      medium: {
-        fontSize: '1.5rem',
-        fontWeight: ({ typography }) => typography.fontWeight.semibold,
-        fontFamily: ({ typography }) => typography.heading.fontFamily,
-        lineHeight: ({ typography }) => typography.heading.lineHeight,
-      },
-      small: {
-        fontSize: '1.3333rem',
-        fontWeight: ({ typography }) => typography.fontWeight.semibold,
-        fontFamily: ({ typography }) => typography.heading.fontFamily,
-        lineHeight: ({ typography }) => typography.heading.lineHeight,
-      },
-    },
-    body: {
-      fontFamily: ({ typography }) => typography.fontFamily,
-      large: {
-        fontSize: '1.5rem',
-        fontWeight: ({ typography }) => typography.body.normal.fontWeight,
-        fontFamily: ({ typography }) => typography.body.normal.fontFamily,
-        lineHeight: '1.75rem',
-      },
-      normal: {
-        fontSize: '1.3333rem',
-        fontWeight: ({ typography }) => typography.fontWeight.regular,
-        fontFamily: ({ typography }) => typography.body.fontFamily,
-        lineHeight: '2rem',
-      },
-      normalBold: {
-        fontSize: ({ typography }) => typography.body.normal.fontSize,
-        fontWeight: ({ typography }) => typography.fontWeight.bold,
-        fontFamily: ({ typography }) => typography.body.normal.fontFamily,
-        lineHeight: ({ typography }) => typography.body.normal.lineHeight,
-      },
-    },
-    label: {
-      fontFamily: ({ typography }) => typography.fontFamily,
-      regular: {
-        fontSize: '1.1667rem',
-        fontWeight: ({ typography }) => typography.fontWeight.regular,
-        fontFamily: ({ typography }) => typography.label.fontFamily,
-        lineHeight: '1.6667rem',
-      },
-      semibold: {
-        fontSize: ({ typography }) => typography.label.regular.fontSize,
-        fontWeight: ({ typography }) => typography.fontWeight.semibold,
-        fontFamily: ({ typography }) => typography.label.regular.fontFamily,
-        lineHeight: ({ typography }) => typography.label.regular.lineHeight,
-      },
-      italics: {
-        fontSize: ({ typography }) => typography.label.regular.fontSize,
-        fontWeight: ({ typography }) => typography.label.regular.fontWeight,
-        fontFamily: ({ typography }) => typography.label.regular.fontFamily,
-        lineHeight: ({ typography }) => typography.label.regular.lineHeight,
-        fontStyle: 'italic',
-      },
-      caption: {
-        fontSize: '1rem',
-        fontWeight: ({ typography }) => typography.fontWeight.regular,
-        fontFamily: ({ typography }) => typography.label.fontFamily,
-        lineHeight: '1.3333rem',
-      },
-      captionBold: {
-        fontSize: ({ typography }) => typography.label.caption.fontSize,
-        fontWeight: ({ typography }) => typography.fontWeight.semibold,
-        fontFamily: ({ typography }) => typography.label.caption.fontFamily,
-        lineHeight: ({ typography }) => typography.label.caption.lineHeight,
-      },
-      inputCaption: {
-        fontSize: '0.8333rem',
-        fontWeight: ({ typography }) => typography.fontWeight.regular,
-        fontFamily: ({ typography }) => typography.label.fontFamily,
-        lineHeight: '1rem',
-      },
-    },
+    // heading1: {
+    //   fontSize: '1.85714em',
+    //   fontWeight: '600',
+    //   fontFamily: ({ typography }) => typography.headings.fontFamily,
+    //   lineHeight: ({ typography }) => typography.lineHeight,
+    //   color: ({ colors }) => colors.primary.main,
+    //   capitalize: true,
+    // },
+    // heading2: {
+    //   fontSize: '1.57143em',
+    //   fontWeight: '600',
+    //   color: ({ colors }) => colors.text.primary,
+    //   fontFamily: ({ typography }) => typography.headings.fontFamily,
+    //   lineHeight: ({ typography }) => typography.lineHeight,
+    //   capitalize: false,
+    // },
+    // heading3: {
+    //   fontSize: '1.27em',
+    //   fontWeight: '600',
+    //   color: ({ colors }) => colors.text.primary,
+    //   fontFamily: ({ typography }) => typography.headings.fontFamily,
+    //   lineHeight: ({ typography }) => typography.lineHeight,
+    //   capitalize: false,
+    // },
+    // heading4: {
+    // // ...
+    // },
+    // heading5: {
+    // // ...
+    // },
+    // heading6: {
+    // // ...
+    // },
     code: {
       fontSize: '14px',
       fontFamily: 'Courier, monospace',
-      fontWeight: ({ typography }) => typography.fontWeight.regular,
+      fontWeight: ({ typography }) => typography.fontWeightRegular,
       color: '#e53935',
       backgroundColor: 'rgba(38, 50, 56, 0.04)',
       wrap: false,
+    },
+    links: {
+      color: ({ colors }) => colors.primary.main,
+      visited: ({ typography }) => typography.links.color,
+      hover: ({ typography }) => lighten(0.2, typography.links.color),
     },
   },
   rightPanel: {
     backgroundColor: '#263238',
     width: '40%',
+    // textColor: '#ffffff',
   },
   schema: {
     nestedBackground: '#fafafa',
+    // linesColor: theme => lighten( theme.colors.tonalOffset, desaturate(theme.colors.tonalOffset, theme.colors.primary.main) ),
+    // defaultDetailsWidth: '75%',
+    // typeNameColor: theme => theme.colors.text.secondary,
+    // typeTitleColor: theme => theme.schema.typeNameColor,
+    // requireLabelColor: theme => theme.colors.error.main,
+    // labelsTextSize: '0.9em',
+    // nestingSpacing: '1em',
+    // arrow: {
+    //   size: '1.1em',
+    //   color: theme => theme.colors.text.secondary,
+    // },
   },
+  // codeBlock: {
+  //   backgroundColor: ({ rightPanel }) => darken(0.1, rightPanel.backgroundColor),
+  //   tokens: {},
+  // },
 };
